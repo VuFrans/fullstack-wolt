9e9dbc6b7e0481afcd72b60a33b15d682bd2b255
Vu Nguyen <vuu.ng97@gmail.com>
2020-05-12 23:46:34 +0200

    Fetch data from Api and display data on frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './styles/App.css';
 
-function App() {
+export default function App() {
+  const URL = 'http://localhost:8000/api/restaurants';
+
+  const [state, setState] = useState([]);
+
+  useEffect(() => {
+    getRestaurants();
+  }, []);
+
+  const getRestaurants = async () => {
+    const response = await axios.get(URL);
+    const data = response.data;
+    setState(data);
+  };
+
   return (
     <div className="App">
       <h1>Fullstack Project</h1>
+      {state.map((restaurant, i) => (
+        <div>
+          <img
+            src={restaurant.image}
+            alt={restaurant.name}
+            style={{ height: 200, width: 300 }}
+          />
+          <p>{restaurant.name}</p>
+        </div>
+      ))}
     </div>
   );
 }
-
-export default App;
