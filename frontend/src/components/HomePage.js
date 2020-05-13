50294003544e4b4c2f350959f5f532b7062d36bf
Vu Nguyen <vuu.ng97@gmail.com>
2020-05-13 19:01:26 +0200

    Add searchbar to frontend

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Card from './Card';
 import '../styles/HomePage.css';
+import TextField from '@material-ui/core/TextField';
 
 export default function HomePage() {
   const URL = 'http://localhost:8000/api/restaurants';
 
   const [state, setState] = useState([]);
+  const [restaurants, setRestaurants] = useState([]);
 
   useEffect(() => {
     getRestaurants();
@@ -16,14 +18,42 @@ export default function HomePage() {
     const response = await axios.get(URL);
     const data = response.data;
     setState(data);
+    setRestaurants(data);
+  };
+
+  const filterRestaurants = (value) => {
+    const hasValue = value.trim();
+
+    if (hasValue) {
+      const filteredRestaurants = state.filter((restaurant) =>
+        restaurant.name.toLowerCase().includes(value.toLowerCase())
+      );
+      setState(filteredRestaurants);
+    } else {
+      setState(restaurants);
+    }
   };
 
   return (
     <div className="root">
-      <h1>Fullstack Project</h1>
-      {state.map((restaurant, i) => (
-        <Card key={i} restaurant={restaurant} />
-      ))}
+      <div className="header">
+        <h1>Fullstack Project</h1>
+        <div style={{ paddingTop: 20 }}>
+          <TextField
+            size="small"
+            type="text"
+            id="outlined-basic"
+            label="Restaurant"
+            variant="outlined"
+            onChange={(event) => filterRestaurants(event.target.value)}
+          />
+        </div>
+      </div>
+      <div className="grid-container">
+        {state.map((restaurant, i) => (
+          <Card key={i} restaurant={restaurant} />
+        ))}
+      </div>
     </div>
   );
 }
