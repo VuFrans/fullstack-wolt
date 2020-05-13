c5e43d5b685a8c5e42c3082b6ca5380c1f78f17d
Vu Nguyen <vuu.ng97@gmail.com>
2020-05-13 14:28:46 +0200

    Add react router to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import HomePage from './components/HomePage';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 export default function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/(|home)" component={HomePage} />
+        <Route render={() => <h1>Page not found</h1>} />
+      </Switch>
+    </BrowserRouter>
   );
 }
