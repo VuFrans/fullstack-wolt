a056de2e1530836a2c4cbf5ef3a620a28c74dfc5
Vu Nguyen <vuu.ng97@gmail.com>
2020-05-16 23:12:47 +0200

    Modularize filterbar component on frontend

---
+++ frontend/src/components/FilterBar.js
@@ -0,0 +1,70 @@
+import React, { Fragment, useState } from 'react';
+import { Button } from '@material-ui/core';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import FormControl from '@material-ui/core/FormControl';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+
+export default function FilterBar(props) {
+  const {
+    tags,
+    removeFilters,
+    filterRestaurantsByTags,
+    handleOpen,
+    open,
+    selected,
+    setSelected,
+  } = props;
+
+  return (
+    <Fragment>
+      {selected.length <= 0 ? (
+        <Button variant="outlined" size="large" onClick={handleOpen}>
+          Suodattimet
+        </Button>
+      ) : (
+        <Button
+          variant="outlined"
+          color="secondary"
+          size="large"
+          onClick={removeFilters}
+        >
+          Poista suodattimet
+        </Button>
+      )}
+      <Dialog open={open} onClose={removeFilters} fullWidth="md">
+        <DialogTitle id="form-dialog-title">Ruokatyylit</DialogTitle>
+        <DialogContent>
+          <FormControl>
+            <FormGroup>
+              {tags.map((tag, index) => (
+                <FormControlLabel
+                  key={index}
+                  control={
+                    <Checkbox
+                      name={tag}
+                      onChange={() => setSelected([...selected, tag])}
+                    />
+                  }
+                  label={tag}
+                />
+              ))}
+            </FormGroup>
+          </FormControl>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={removeFilters} color="secondary">
+            Peruuta
+          </Button>
+          <Button onClick={filterRestaurantsByTags} color="primary">
+            Hae
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </Fragment>
+  );
+}
--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -4,15 +4,7 @@ import Card from './Card';
 import '../styles/HomePage.css';
 import TextField from '@material-ui/core/TextField';
 import Fab from './ScrollTopButton';
-import { Button } from '@material-ui/core';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import FormControl from '@material-ui/core/FormControl';
-import FormGroup from '@material-ui/core/FormGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
+import FilterBar from './FilterBar';
 
 export default function HomePage() {
   const URL = 'http://localhost:8000/api/restaurants';
@@ -53,16 +45,11 @@ export default function HomePage() {
       selected.some((tag) => restaurant.tags.includes(tag))
     );
     setState(filterTag);
-    setOpen(false);
+    handleClose();
   };
 
-  const handleClick = () => {
-    if (!open) {
-      setOpen(true);
-    } else {
-      setOpen(false);
-    }
-  };
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   const getRestaurantsTags = async () => {
     const response = await axios.get(`${URL}/tags`);
@@ -73,7 +60,7 @@ export default function HomePage() {
   const removeFilters = () => {
     setState(restaurants);
     setSelected([]);
-    open && setOpen(false);
+    handleClose();
   };
 
   return (
@@ -89,49 +76,16 @@ export default function HomePage() {
             variant="outlined"
             onChange={(event) => filterRestaurants(event.target.value)}
           />
-          {selected.length <= 0 ? (
-            <Button variant="outlined" size="large" onClick={handleClick}>
-              Suodattimet
-            </Button>
-          ) : (
-            <Button
-              variant="outlined"
-              color="secondary"
-              size="large"
-              onClick={removeFilters}
-            >
-              Poista suodattimet
-            </Button>
-          )}
-          <Dialog open={open} onClose={removeFilters} fullWidth="md">
-            <DialogTitle id="form-dialog-title">{'Ruokatyylit'}</DialogTitle>
-            <DialogContent>
-              <FormControl>
-                <FormGroup>
-                  {tags.map((tag, index) => (
-                    <FormControlLabel
-                      key={index}
-                      control={
-                        <Checkbox
-                          name={tag}
-                          onChange={() => setSelected([...selected, tag])}
-                        />
-                      }
-                      label={tag}
-                    />
-                  ))}
-                </FormGroup>
-              </FormControl>
-            </DialogContent>
-            <DialogActions>
-              <Button onClick={removeFilters} color="secondary">
-                Peruuta
-              </Button>
-              <Button onClick={filterRestaurantsByTags} color="primary">
-                Hae
-              </Button>
-            </DialogActions>
-          </Dialog>
+          <FilterBar
+            tags={tags}
+            removeFilters={removeFilters}
+            filterRestaurantsByTags={filterRestaurantsByTags}
+            handleOpen={handleOpen}
+            handleClose={handleClose}
+            open={open}
+            selected={selected}
+            setSelected={setSelected}
+          />
         </div>
       </div>
       <div className="grid-container">
